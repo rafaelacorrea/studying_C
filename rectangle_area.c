6db748f0a059151aee97c15779a8db8740ddfb99
rafaelacorrea <rafaela.f.c.correa@gmail.com>
2024-03-20 22:18:50 -0300

    refactor: :recycle: changing code language to english

--- rectangle_area.c
+++ rectangle_area.c
@@ -1,12 +1,12 @@
 //calculating the rectangle area:
 #include <stdio.h>
 int main(){
-    float base, altura;
-    printf("Digite o valor da base do retângulo: \n");
+    float base, heigh;
+    printf("Type a value for the rectangle base: \n");
     scanf("%f", &base);
-    printf("Digite o valor da altura do retângulo: \n");
-    scanf("%f", &altura);
-    printf("A área desse retângulo é: %fcm²\n", base * altura);
+    printf("Type a value for the rectangle heigh: \n");
+    scanf("%f", &heigh);
+    printf("The rectangle area is: %fcm²\n", base * heigh);
 
     return 0;
 }
